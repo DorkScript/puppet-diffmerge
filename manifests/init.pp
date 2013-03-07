c93669bfc0a448e30f27a27accf6a12995646606
Arron Green <arrong@gmail.com>
2013-03-07 11:01:35 -0500

    added the download link for 3.3.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,6 @@ class diffmerge{
   package { 'diffmerge':
     provider => 'pkgdmg',
     ensure   => 'installed',
-    source   => 'http://sourcegar.com/DiffMerge.3.3.2.1139.pkg',
+    source   => 'http://download-us.sourcegear.com/DiffMerge/3.3.2/DiffMerge.3.3.2.1139.pkg',
   }
 }
--- spec/classes/diffmerge_spec.rb
+++ spec/classes/diffmerge_spec.rb
@@ -3,7 +3,7 @@ describe 'diffmerge' do
   it do
     should contain_package('diffmerge').with({
       :provider => 'pkgdmg',
-      :source => 'http://sourcegar.com/DiffMerge.3.3.2.1139.pkg'
+      :source => 'http://download-us.sourcegear.com/DiffMerge/3.3.2/DiffMerge.3.3.2.1139.pkg'
     })
   end
 end
