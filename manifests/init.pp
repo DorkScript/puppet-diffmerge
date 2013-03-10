d46c76a166cf77bb09f5c3a29c119f793df7386e
Arron Green <arrong@gmail.com>
2013-03-10 17:42:12 -0400

    added template for writing diffmerge script

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,4 +9,13 @@ class diffmerge{
     ensure   => 'installed',
     source   => 'http://download-us.sourcegear.com/DiffMerge/3.3.2/DiffMerge.3.3.2.1139.dmg',
   }
+
+  file { 'diffmerge_script' :
+    path    => '/usr/bin/diffmerge',
+    content => template('diffmerge/diffmerge.sh.erb'),
+    require => Package['diffmerge'],
+    mode    => 644,
+    owner   => root,
+    group   => wheel,
+  }
 }
